5e97eb3106a2372140ddb4e400bbf0503dd8b286
Joanna Gorska <gorska919@gmail.com>
2021-07-19 22:01:55 +0000

    Add all code from love maths to check if I can adjust it to my needs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,149 @@
-//remember to use ID male and female from welcome question to pre fill the form in calculator
 
-// function - Replace questions Loose Weight -> Excercise More
-// I need one function to replace HTML for male and female and two functions to pre fill the form in calculator 
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
+document.addEventListener("DOMContentLoaded", function() {
+    
+    let buttons = document.getElementsByTagName("button");
+    
+    for(let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        })
+    }
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key ==="Enter") {
+            checkAnswer();
+        }
 
-document.getElementById('male').addEventListener("click", function() {
-    let excerciseMoreVar =document.getElementsByClassName("welcome-container")[0];
+    }
+    
+    )
 
-    excerciseMoreVar.innerHTML = `
+    runGame("addition");
+});
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 
-        <div class="excercise-container">
-        <div class="big-welcome">Do you want to excercise More?</div>
-        <div class="answer-container">
-            <div id="yes-excercise" class="yes-answer my-col-6">Yes</div>
-            <div id="no-excercise" class="no-answer my-col-6">No</div>
-        </div>
-        </div>
-        `
-})
+function runGame(gameType) {
 
-document.getElementById('female').addEventListener("click", function() {
-    let excerciseMoreVar =document.getElementsByClassName("welcome-container")[0];
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
-    excerciseMoreVar.innerHTML = `
+    // Creates two random numbers 1 - 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
-        <div class="excercise-container">
-        <div class="big-welcome">Do you want to excercise More?</div>
-        <div class="answer-container">
-            <div id="yes-excercise" class="yes-answer my-col-6">Yes</div>
-            <div id="no-excercise" class="no-answer my-col-6">No</div>
-        </div>
-        </div>
-        `
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
 
-        
-})
+    } else if (gameType === "substract") {
+        displaySubtractQuestion(num1, num2);
 
-document.getElementById("yes-excercise").addEventListener("click", function(){
-    console.log("inside honest function");
-        let honestDiv = document.getElementByClassName(`excercise-container`);
-        honestDiv.innerHTML = 
-        `<div class="answer-container">
-            <p>Please can you be honest with me?</p>
-            <button>Go Back</button>
-        </div>`
+    } else if (gameType === "division") {
+        displayDivideQuestion(num1, num2);
+
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
-)
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
+
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+            alert("Hey! You got it right! :D")
+            incrementScore();
+    }else {
+        alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
+    }
+    runGame(calculatedAnswer[1]);
+}
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the DOM, and returns the correct answer.
+ */
+function calculateCorrectAnswer() {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+
+    } else if (operator === "-") {
+        return [operand1 - operand2, "substract"];
+
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}, Aborting!`
+    }
+    
+}
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+}
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+}
+
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+}
+
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivideQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
+
+}
+
+

a2a8be854c9521aa859d2c4b2179d1902ec36205
Joanna Gorska <gorska919@gmail.com>
2021-07-19 18:33:05 +0000

    add function to replace html when yes to excercise more

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 //remember to use ID male and female from welcome question to pre fill the form in calculator
 
 // function - Replace questions Loose Weight -> Excercise More
+// I need one function to replace HTML for male and female and two functions to pre fill the form in calculator 
+
+
 document.getElementById('male').addEventListener("click", function() {
     let excerciseMoreVar =document.getElementsByClassName("welcome-container")[0];
 
@@ -29,10 +32,17 @@ document.getElementById('female').addEventListener("click", function() {
         </div>
         </div>
         `
+
+        
 })
 
-document.getElementById('yes-excercise').addEventListener("clic", function(){
-
-    
+document.getElementById("yes-excercise").addEventListener("click", function(){
+    console.log("inside honest function");
+        let honestDiv = document.getElementByClassName(`excercise-container`);
+        honestDiv.innerHTML = 
+        `<div class="answer-container">
+            <p>Please can you be honest with me?</p>
+            <button>Go Back</button>
+        </div>`
 }
 )

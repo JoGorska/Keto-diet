1f678181150357a19cdceae41f1cac694f10a922
Joanna Gorska <gorska919@gmail.com>
2021-07-19 18:02:05 +0000

    Add excercise-more question in the welcome questions set

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,38 @@
 //remember to use ID male and female from welcome question to pre fill the form in calculator
+
+// function - Replace questions Loose Weight -> Excercise More
 document.getElementById('male').addEventListener("click", function() {
-    let excerciseMore =document.getElementsByClassName("welcome-container")[0];
+    let excerciseMoreVar =document.getElementsByClassName("welcome-container")[0];
 
-    excerciseMore.innerHTML = `
+    excerciseMoreVar.innerHTML = `
 
-    <div class="excercise-container">
-    <div class="big-welcome">Do you want to excercise More?</div>
-    <div class="answer-container">
-        <div class="yes-answer">Yes</div>
-        <div class="no-answer">No</div>
-    </div>
-    </div>
-    `
+        <div class="excercise-container">
+        <div class="big-welcome">Do you want to excercise More?</div>
+        <div class="answer-container">
+            <div id="yes-excercise" class="yes-answer my-col-6">Yes</div>
+            <div id="no-excercise" class="no-answer my-col-6">No</div>
+        </div>
+        </div>
+        `
+})
+
+document.getElementById('female').addEventListener("click", function() {
+    let excerciseMoreVar =document.getElementsByClassName("welcome-container")[0];
+
+    excerciseMoreVar.innerHTML = `
+
+        <div class="excercise-container">
+        <div class="big-welcome">Do you want to excercise More?</div>
+        <div class="answer-container">
+            <div id="yes-excercise" class="yes-answer my-col-6">Yes</div>
+            <div id="no-excercise" class="no-answer my-col-6">No</div>
+        </div>
+        </div>
+        `
+})
+
+document.getElementById('yes-excercise').addEventListener("clic", function(){
+
+    
 }
-
-) 
+)

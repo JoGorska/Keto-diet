fca4e206ecc7ee0b6af8d2984031e47c72a89c5b
Joanna Gorska <gorska919@gmail.com>
2021-07-20 12:38:25 +0000

    Remove the functions checking the answer and cacluating the right answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
+
 document.addEventListener("DOMContentLoaded", function() {
     
     let buttons = document.getElementsByTagName("button");
@@ -9,18 +10,31 @@ document.addEventListener("DOMContentLoaded", function() {
     for(let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "wrong-answer") {
+                
                 console.log("wrong-aswer add function");
+                document.getElementById("answer1").addEventListener("click", hondestDiv);
+
+                function honestDiv() {
+                    document.getElementsByClassName("welcome-container")[0].innerHTML = `
+                    <div class="welcome-container">
+                    <div id="question" class="big-welcome">I need you to be honest with me</div>
+                    <div class="answer-container">
+                        <button data-type="weight" id="go-back" >Go Back</button>
+                    </div>
+                    </div>
+                    `
+                }
             } else {
                 let questionType = this.getAttribute("data-type");
                 runGame(questionType);
             }
         })
     }
-    runGame("weight");
+    
 });
 /**
  * The main game "loop", called when the script is first loaded
- * and after the user's answer has been processed
+ * depending on data-type attribute the game should display different question
  */
 
 function runGame(questionType) {
@@ -42,111 +56,6 @@ function runGame(questionType) {
         alert(`unknown question type: ${questionType}`);
         throw `Unknown question type: ${questionType}. Aborting!`;
     }
-}
-/**
- * Checks the answer against the first element in
- * the returned loadNextQuestion array
- */
-
-function checkAnswer() {
-    let answer1 = document.getElementById("answer1").innerText;
-    if (answer1 === "Yes") {
-        document.getElementById("answer1").addEventListener("click", hondestDiv);
-        function honestDiv() {
-            document.getElementsByClassName("welcome-container")[0].innerHTML = `
-            <div class="welcome-container">
-            <div id="question" class="big-welcome">I need you to be honest with me</div>
-            <div class="answer-container">
-                <button data-type="weight" id="go-back" >Go Back</button>
-            </div>
-            </div>
-            `
-            document.getElementById("go-back").addEventListener("click", runGame("weight"));
-
-   // } else {
-
-        document.getElementById("answer1").addEventListener("click", loadNextQuestion);
-            
-    }
-}
-    
-    document.getElementById("answer1").addEventListener("click", );
-    let calculatedAnswer = loadNextQuestion();
-    let isCorrect = userAnswer === calculatedAnswer[0];
-    let 
-
-
-    if (isCorrect) {
-        console.log("correct answer, next question please");
-    }else {
-        console.log("incorrect, get the honestDiv");
-    }
-    runGame(calculatedAnswer[1]);
-}
-/**
- * Gets the answers and the questions (plus, minus etc)
- * directly from the DOM, and returns new HTML.
- */
-function loadNextQuestion() {
-
-    let question = document.getElementById('question').innerText;
-
-    if (question === 'Do you want to Loose Weight?') {
-        return [
-            document.getElementsByClassName("welcome-container")[0].innerHTML = `
-            <div class="welcome-container">
-            <div data-type="weight" id="question" class="big-welcome">Do you want to loose weight?</div>
-            <div class="answer-container">
-                <div class="my-col-6" ><button data-type="next-question" id="answer1" data-type="male">Male</button></div>
-                <div class="my-col-6"><button data-type="next-question" id="answer2" data-type="female">Female</button></div>
-            </div>
-            </div>
-            `
-        ];
-
-    } else if (question === "Do you want to excercise More?") {
-        return [
-            document.getElementsByClassName("welcome-container")[0].innerHTML = `
-            <div class="welcome-container">
-            <div data-type="excercise" id="question" class="big-welcome">Do you want to excercise More?</div>
-            <div class="answer-container">
-                <div class="my-col-6" ><button data-type="wrong-answer" id="answer1">Yes</button></div>
-                <div class="my-col-6"><button data-type="next-question" id="answer2">No</button></div>
-            </div>
-            </div>
-            `
-        ];
-
-    } else if (question === 'Do you want to calculate every Calorie you consume?') {
-        return [
-            document.getElementsByClassName("welcome-container")[0].innerHTML = `
-            <div class="welcome-container">
-            <div data-type="calories" id="question" class="big-welcome">Do you want to calculate every Calorie you consume?</div>
-            <div class="answer-container">
-                <div class="my-col-6" ><button data-type="wrong-answer" id="answer1">Yes</button></div>
-                <div class="my-col-6"><button data-type="next-question" id="answer2">No</button></div>
-            </div>
-            </div>
-            `
-    ];
-
-    } else if (question === 'Calculate what you can acheve with Keto Diet:') {
-        return [
-        document.getElementsByClassName("welcome-container")[0].innerHTML = `
-        <div class="welcome-container">
-        <div data-type="which-calculator" id="question" class="big-welcome">Calculate what you can acheve with Keto Diet:</div>
-        <div class="answer-container">
-            <div class="my-col-6" ><button data-type="target-weight" id="answer1">How soon I can acheve my weight goal?</button></div>
-            <div class="my-col-6"><button data-type="hard-deadline" id="answer2">How much I can loose untill my hard deadline?</button></div>
-        </div>
-        </div>
-        `
-        ];
-    } else {
-        alert(`Unimplemented question ${question}`);
-        throw `Unimplemented question ${question}, Aborting!`
-    }
-    
 }
 
 

e8fb40bc09d4d0c6267f5c09960f7a5616c972b5
Joanna Gorska <gorska919@gmail.com>
2021-07-21 20:49:39 +0000

    add loop to respond to button clicks in Excercise Question

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,13 @@
 
 document.getElementById('learn-more').addEventListener("click", function() {
     let mainContainer =document.getElementById("main-container");
-
+    
+    //Question about excercise
     mainContainer.innerHTML = `
 
     <div class="px-4 py-5 my-5 text-center">
     <img class="d-block mx-auto mb-4" src="#" alt="little logo" width="72" height="57">
-    <h1 class="display-5 fw-bold">Do you want to excercise more?</h1>
+    <h1 class="display-5 fw-bold">Do you want to excercise More?</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4"></p>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
@@ -22,8 +23,24 @@ document.getElementById('learn-more').addEventListener("click", function() {
     </div>
   </div>
   `
-});
+  let buttons = document.getElementsByTagName("button");
+  for(let button of buttons) {
+    button.addEventListener("click", function() {
+        if (this.innerHTML === "Yes") {
+            console.log("I clicked Yes")
+            //Response to Yes answer, yet to be written
 
+        } else if (this.innerHTML === "No") {
+            console.log("I clicked No");
+            
+
+        } else {
+          console.log("button unnknown")
+        }
+
+    })
+  };
+});
 
 
 

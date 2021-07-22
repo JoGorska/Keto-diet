b7e334933c48ce8cda2ba21b4795611f33003f32
Joanna Gorska <gorska919@gmail.com>
2021-07-22 08:55:09 +0000

    Fix indentation in the script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,28 +6,28 @@
 
 
 document.getElementById('learn-more').addEventListener("click", function() {
-    let mainContainer =document.getElementById("main-container");
-    
-    //Excercise Question
-    mainContainer.innerHTML = `
+  let mainContainer =document.getElementById("main-container");
+  
+  //Excercise Question
+  mainContainer.innerHTML = `
 
     <div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="#" alt="little logo" width="72" height="57">
-    <h1 id="excercise-question" class="display-5 fw-bold">Do you want to excercise More?</h1>
-    <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4"></p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <button type="button" class="btn btn-danger btn-lg px-4 gap-3">Yes</button>
-        <button type="button" class="btn btn-success btn-lg px-4">No</button>
+      <img class="d-block mx-auto mb-4" src="#" alt="little logo" width="72" height="57">
+      <h1 id="excercise-question" class="display-5 fw-bold">Do you want to excercise More?</h1>
+      <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4"></p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <button type="button" class="btn btn-danger btn-lg px-4 gap-3">Yes</button>
+          <button type="button" class="btn btn-success btn-lg px-4">No</button>
+        </div>
       </div>
-    </div>
-  </div>
-  `
+    </div>`
+    
   //Loop through all buttons responding to yes and no
 
   let buttons = document.getElementsByTagName("button");
-  for(let button of buttons) {
-    button.addEventListener("click", function() {
+    for(let button of buttons) {
+      button.addEventListener("click", function() {
         if (this.innerHTML === "Yes") {
             console.log("I clicked Yes")
             //Response to Yes answer, yet to be written
@@ -36,63 +36,64 @@ document.getElementById('learn-more').addEventListener("click", function() {
             console.log("I clicked No");
 
             let mainContainer =document.getElementById("main-container");
+
             //Calorie Question 
             mainContainer.innerHTML = `
-        
-            <div class="px-4 py-5 my-5 text-center">
-              <img class="d-block mx-auto mb-4" src="#" alt="little logo" width="72" height="57">
-              <h1 id="calorie-question" class="display-5 fw-bold">Do you want to calculate Every calorie?</h1>
-              <div class="col-lg-6 mx-auto">
-                <p class="lead mb-4"></p>
-                <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                  <button type="button" class="btn btn-danger btn-lg px-4 gap-3">Yes</button>
-                  <button type="button" class="btn btn-success btn-lg px-4">No</button>
+              <div class="px-4 py-5 my-5 text-center">
+                <img class="d-block mx-auto mb-4" src="#" alt="little logo" width="72" height="57">
+                <h1 id="calorie-question" class="display-5 fw-bold">Do you want to calculate Every calorie?</h1>
+                <div class="col-lg-6 mx-auto">
+                  <p class="lead mb-4"></p>
+                  <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                    <button type="button" class="btn btn-danger btn-lg px-4 gap-3">Yes</button>
+                    <button type="button" class="btn btn-success btn-lg px-4">No</button>
+                  </div>
                 </div>
-              </div>
-            </div>`  
+              </div>` 
+
             //new loop for buttons after HTML has been changed
             let buttons = document.getElementsByTagName("button");
             for(let button of buttons) {
               button.addEventListener("click", function() {
 
               if (this.innerHTML === "Yes") {
-                  console.log("I clicked Yes, no problem");
+                console.log("I clicked Yes, to calorie count");
 
-                  let mainContainer =document.getElementById("main-container");
-                  //Calorie Question Yes, no problem response yet to be written
-                  mainContainer.innerHTML = ''
+                let mainContainer =document.getElementById("main-container");
+
+                //Calorie Question Answer: Yes, response yet to be written
+                mainContainer.innerHTML = ''
             
 
               } else if (this.innerHTML === "No") {
-                  console.log("I clicked No, far too dificult");
+                console.log("I clicked No to calorie count");
 
-                  let mainContainer =document.getElementById("main-container");
+                let mainContainer =document.getElementById("main-container");
                   //Calculator Question 
-                  mainContainer.innerHTML = `
-              
-                  <div class="px-4 py-5 my-5 text-center">
-                    <img class="d-block mx-auto mb-4" src="#" alt="little logo" width="72" height="57">
-                    <h1 id="calorie-question" class="display-5 fw-bold">Calculator</h1>
-                    <div class="col-lg-6 mx-auto">
-                      <p class="lead mb-4"></p>
-                      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                        <button type="button" class="btn btn-danger btn-lg px-4 gap-3">Target Weight</button>
-                        <button type="button" class="btn btn-success btn-lg px-4">Target Date</button>
+                mainContainer.innerHTML = `
+                
+                    <div class="px-4 py-5 my-5 text-center">
+                      <img class="d-block mx-auto mb-4" src="#" alt="little logo" width="72" height="57">
+                      <h1 id="calorie-question" class="display-5 fw-bold">Calculator</h1>
+                      <div class="col-lg-6 mx-auto">
+                        <p class="lead mb-4"></p>
+                        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                          <button type="button" class="btn btn-danger btn-lg px-4 gap-3">Target Weight</button>
+                          <button type="button" class="btn btn-success btn-lg px-4">Target Date</button>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                  `
+                    </div>`
               } else {
                   console.log("button unnknown")
-                }
-            })
+              };
+            });
           }
         } else {
           console.log("button unnknown")
         }
 
-    })
-  };
+      })
+    };
 });
 
 

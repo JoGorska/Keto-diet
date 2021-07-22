c930253c906aca2d074a65ff874cf2ed35ab828f
Joanna Gorska <gorska919@gmail.com>
2021-07-22 18:09:55 +0000

    Add new content to last welcome question and fix error in index with title

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,9 +73,9 @@ document.getElementById('learn-more').addEventListener("click", function() {
                 
                     <div class="px-4 py-5 my-5 text-center">
                       <img class="d-block mx-auto mb-4" src="#" alt="little logo" width="72" height="57">
-                      <h1 id="calorie-question" class="display-5 fw-bold">Calculator</h1>
+                      <h1 id="calorie-question" class="display-5 fw-bold">Predicted Result Calculator</h1>
                       <div class="col-lg-6 mx-auto">
-                        <p class="lead mb-4"></p>
+                        <p class="lead mb-4">See what you can acheve by changing your fuel from carbohydrates to fat (yes FAT)</p>
                         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
                           <a href="calculator.html"><button type="button" class="btn btn-danger btn-lg px-4 gap-3">Target Weight</button></a>
                           <a href="calculator.html"><button type="button" class="btn btn-success btn-lg px-4">Target Date</button></a>

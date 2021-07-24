5b1743108b7c65f6c0b0f06403243c9c46faf823
Joanna Gorska <gorska919@gmail.com>
2021-07-24 14:06:59 +0000

    Removed Whole Calculator Form from scirpt.js and changed nav bar and Welcome question button to direct to calculator.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,8 +77,8 @@ document.getElementById('learn-more').addEventListener("click", function() {
                       <div class="col-lg-6 mx-auto">
                         <p class="lead mb-4">See what you can acheve by changing your fuel from carbohydrates to fat (yes FAT)</p>
                         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                          <button id="target-weight" type="button" class="btn btn-danger btn-lg px-4 gap-3">Target Weight</button>
-                          <button id="target-date" type="button" class="btn btn-success btn-lg px-4">Target Date</button>
+                          <a href="calculator.html"><button id="target-weight" type="button" class="btn btn-danger btn-lg px-4 gap-3">Calculator</button></a>
+                          
                         </div>
                       </div>
                     </div>`
@@ -149,132 +149,14 @@ document.getElementById("main-container").addEventListener("click", function(eve
  * Main function to create calculator form for Target Date option with Metric measures
  */
  function calcFormTargetDatetMetric() {
-  removeClassH100();
+  
    return (
-    document.getElementById("main-container").innerHTML = 
+    console.log("toggle radio button for date")
  // full HTML of the calculator goes into main-container
- //BUG ??? need to add nav bar because it displays home...   
-`
-<div class="container">
-      
-    <div class="p-5 pb-md-4 mx-auto text-center">
-        <h1 id="booking" class="display-4 fw-normal border-top p-5 font-family-header border-success">Calculator</h1>
-        <p class="fs-5 text-muted pb-5 border-bottom border-success">Choose from one of the options below to calculate how long will it take you to acheve the target weight or how much weight you can loose untill your target date</p>
-    </div>
-        
-</div>
+   
 
-<!--Calculator Form-->
-<form id="calcFormTargetDate" action="https://formdump.codeinstitute.net/" method="POST" target="_blank">
-    <div class="container p-3 mb-3">
-        
-        <!--
-        Choose Target Weight or Target Date
-        Button group copied from Bootswatch and applied bootsstrap and bootstwatch styling
-        -->
-
-        <div class="text-center bs-component mb-3">
-            <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-              <input type="radio" class="btn-check" name="target" id="targetWeight" autocomplete="off" >
-              <label class="btn btn-outline-success" for="targetWeight">Target Weight</label>
-              <input type="radio" class="btn-check" name="target" id="targetDate" autocomplete="off" checked required>
-              <label class="btn btn-outline-success" for="targetDate">Target Date</label>
-            </div>
-        </div>
-
-        <!--Name-->
-        <div id="inputNameDiv" class="mb-3">
-            <label for="ipnutName" class="form-label">Name</label>
-            <input type="text" class="form-control" id="inputName" name="name" required>
-        </div>
-
-        <!--Age-->
-        <div class="mb-3">
-            <label for="inputAge" class="form-label">Age</label>
-            <input type="number" class="form-control" id="inputAge" min="18" max="110" name="name" aria-describedby="ageHelp" required>
-            <div id="ageHelp" class="form-text">We'll never share your age with anyone else.</div>
-        </div>
-
-        <!--
-        Choose Imperial Measures and Metric
-        Button group copied from Bootswatch and applied bootsstrap and bootstwatch styling
-        -->
-            
-        <fieldset>
-            <legend class="text-center mb-5">Choose between Impearial Units and Metric</legend>
-            
-            <!--button group code copied from bootswatch and applied styling from bootswatch and bootstrap-->
-            <div class="text-center bs-component mb-3">
-                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                    <input type="radio" class="btn-check" name="unitsOfMeasure" id="imperial" autocomplete="off">
-                    <label class="btn btn-outline-success" for="imperial">Imperial Units</label>
-                    <input type="radio" class="btn-check" name="unitsOfMeasure" id="metric" autocomplete="off" checked="" required>
-                    <label class="btn btn-outline-success" for="metric">Metric</label>
-                </div>
-            </div>
-            
-            <!--Current weight for metric measures-->
-            <div id="swapMeasures" class="container">
-                <div class="mb-3">
-                  <label for="currentWeightKg" class="form-label">Current Weight</label>
-                  <input type="number" class="form-control" id="currentWeightKg" name="currentWeightKg" placeholder="kg">
-                </div>
-                <!--Target date for metric-->
-                <div class="mb-3">
-                  <label for="targetDatetKg" class="form-label">Target Date</label>
-                  <input type="date" class="form-control" id="targetDatetKg" name="targetDatetKg" required>
-                </div>
-                <!--height for metric-->
-              <div class="mb-3">
-                <label for="heightM" class="form-label">Height</label>
-                <input type="number" class="form-control" id="heightM" name="heightM" placeholder="cm" required>
-              </div>
-            </div>
-            
-        </fieldset>
-
-        <!--Hours of excercise per week-->
-        <div class="mb-3">
-            <label for="excerciseHours" class="form-label">Hours of excercise per week</label>
-            <input type="number" class="form-control" id="excerciseHours" name="excerciseHours" required>
-        </div>
-
-        <label class="form-label" for="inputDiet">Your current Diet</label>
-        <input class="form-control mb-3" type="text" name="inputDiet" id="inputDiet" list="dietList" placeholder="Please write here or choose from drop down list" minlength="2" maxlength="160" required>
-
-        <datalist id="dietList">
-            <option>None</option>
-            <option>Keto</option>
-            <option>Low Carb</option>
-            <option>High Protein</option>
-            <option>Eat Less</option>
-            <option>Zryj Mniej</option>
-            <option>Noom</option>
-            <option>NHS program</option>
-            <option>Cabbage Soup Diet</option>
-            <option>Weight Watchers</option>
-            <option>Slimming World</option>
-            <option>Low Fat</option>
-            <option>My Own Diet</option>
-
-        </datalist>
-
-        <!--Buttons at the end of the page-->
-      
-        <div class="container-fluid text-center">
-            <div class="row">
-               <div class="col">
-                    <button type="submit" class="btn btn-success">Submit</button>
-               </div>
-               <div class="col">
-                <button type="reset" class="btn btn-danger">Reset</button>
-               </div> 
-            </div>
-        </div>
-    </div>
-  </form>
     
-  `)
+  )
 };
 
 /**
@@ -283,129 +165,11 @@ document.getElementById("main-container").addEventListener("click", function(eve
 
 
 function calcFormTargetWeightMetric() {
-  removeClassH100();
+  
   return (
-  document.getElementById("main-container").innerHTML = 
+    console.log("toggle radio button to Weight")
+  )
 
-`  <div class="container">
-      
-    <div class="p-5 pb-md-4 mx-auto text-center">
-        <h1 id="booking" class="display-4 fw-normal border-top p-5 font-family-header border-success">Calculator</h1>
-        <p class="fs-5 text-muted pb-5 border-bottom border-success">Choose from one of the options below to calculate how long will it take you to acheve the target weight or how much weight you can loose untill your target date</p>
-    </div>
-        
-</div>
-
-<!--Calculator Form-->
-<form id="calcFormTargetWeight" action="https://formdump.codeinstitute.net/" method="POST" target="_blank">
-    <div class="container p-3 mb-3">
-        
-        <!--
-        Choose Target Weight or Target Date
-        Button group copied from Bootswatch and applied bootsstrap and bootstwatch styling
-        -->
-
-        <div class="text-center bs-component mb-3">
-            <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-              <input type="radio" class="btn-check" name="target" id="targetWeight" autocomplete="off" checked required>
-              <label class="btn btn-outline-success" for="targetWeight">Target Weight</label>
-              <input type="radio" class="btn-check" name="target" id="targetDate" autocomplete="off">
-              <label class="btn btn-outline-success" for="targetDate">Target Date</label>
-            </div>
-        </div>
-
-        <!--Name-->
-        <div id="inputNameDiv" class="mb-3">
-            <label for="ipnutName" class="form-label">Name</label>
-            <input type="text" class="form-control" id="inputName" name="name" required>
-        </div>
-
-        <!--Age-->
-        <div class="mb-3">
-            <label for="inputAge" class="form-label">Age</label>
-            <input type="number" class="form-control" id="inputAge" min="18" max="110" name="name" aria-describedby="ageHelp" required>
-            <div id="ageHelp" class="form-text">We'll never share your age with anyone else.</div>
-        </div>
-
-        <!--
-        Choose Imperial Measures and Metric
-        Button group copied from Bootswatch and applied bootsstrap and bootstwatch styling
-        -->
-            
-        <fieldset>
-            <legend class="text-center mb-5">Choose between Impearial Units and Metric</legend>
-            
-            <!--button group code copied from bootswatch and applied styling from bootswatch and bootstrap-->
-            <div class="text-center bs-component mb-3">
-                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                    <input type="radio" class="btn-check" name="unitsOfMeasure" id="imperial" autocomplete="off">
-                    <label class="btn btn-outline-success" for="imperial">Imperial Units</label>
-                    <input type="radio" class="btn-check" name="unitsOfMeasure" id="metric" autocomplete="off" checked="" required>
-                    <label class="btn btn-outline-success" for="metric">Metric</label>
-                </div>
-            </div>
-                       
-            <!--Current weight for metric measures-->
-            <div id="swapMeasures" class="container">
-                <div class="mb-3">
-                    <label for="currentWeightKg" class="form-label">Current Weight</label>
-                    <input type="number" class="form-control" id="currentWeightKg" name="currentWeightKg" placeholder="kg">
-                </div>
-                <!--Target weight for metric-->
-                <div class="mb-3">
-                    <label for="targetWeightKg" class="form-label">Target Weight</label>
-                    <input type="number" class="form-control" id="targetWeightKg" name="targetWeightKg" placeholder="kg" required>
-                </div>
-                <!--height for metric-->
-              <div class="mb-3">
-                  <label for="heightM" class="form-label">Height</label>
-                  <input type="number" class="form-control" id="heightM" name="heightM" placeholder="cm" required>
-              </div>
-            </div>
-            
-        </fieldset>
-
-        <!--Hours of excercise per week-->
-        <div class="mb-3">
-            <label for="excerciseHours" class="form-label">Hours of excercise per week</label>
-            <input type="number" class="form-control" id="excerciseHours" name="excerciseHours" required>
-        </div>
-
-        <label class="form-label" for="inputDiet">Your current Diet</label>
-        <input class="form-control mb-3" type="text" name="inputDiet" id="inputDiet" list="dietList" placeholder="Please write here or choose from drop down list" minlength="2" maxlength="160" required>
-
-        <datalist id="dietList">
-            <option>None</option>
-            <option>Keto</option>
-            <option>Low Carb</option>
-            <option>High Protein</option>
-            <option>Eat Less</option>
-            <option>Zryj Mniej</option>
-            <option>Noom</option>
-            <option>NHS program</option>
-            <option>Cabbage Soup Diet</option>
-            <option>Weight Watchers</option>
-            <option>Slimming World</option>
-            <option>Low Fat</option>
-            <option>My Own Diet</option>
-
-        </datalist>
-
-        <!--Buttons at the end of the page-->
-      
-        <div class="container-fluid text-center">
-            <div class="row">
-               <div class="col">
-                    <button id="submitButton" type="submit" class="btn btn-success">Submit</button>
-               </div>
-               <div class="col">
-                <button type="reset" class="btn btn-danger">Reset</button>
-               </div> 
-            </div>
-        </div>
-    </div>
-  </form>
-`)
 };
 ////////////////////////////////////////////styling functions///////////////////////////////////////////////
 /**

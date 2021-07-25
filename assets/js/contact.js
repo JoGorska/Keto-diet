a2620051d99308f65b9610367c90fd66afd23675
Joanna Gorska <gorska919@gmail.com>
2021-07-25 19:46:19 +0000

    Add submit event listener and basic function names

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -5,3 +5,42 @@ var email = document.getElementById("email")
 var telephone = document.getElementById("telephone")
 var enquiry = document.getElementById("enquiry")
 
+document.getElementsByTagName("FORM")[0].addEventListener("submit", handleContactSubmit);
+
+function handleContactSubmit(event) {
+    event.preventDefault();
+    if (contactFormValidation() == false) {
+        highlightErrors()
+
+    } else {
+        console.log("all good to go")
+        FormData.submit()
+    }
+
+}
+
+function validateIfEmpty () {
+    this.value = ""
+}
+
+function validateLenght() {
+
+}
+
+function validateOnlyLetters () {
+
+}
+
+//functions to validate each input field
+function validateContactName () {
+    
+}
+
+//function to check if all validations passed
+function contactFormValidation() {
+
+}
+
+function highlightErrors() {
+    console.log("I will add or remove classess")
+}

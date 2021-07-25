3fc3f5e60f8e2285214cfcc5ce62293d315285d5
Joanna Gorska <gorska919@gmail.com>
2021-07-25 13:14:21 +0000

    renamed the script.js to calculator.js to be used for calculator.html

---
+++ assets/js/calculator.js
@@ -0,0 +1,38 @@
+
+
+
+//////////////////////////////////////////////////////////////////Caclulator starts here///////////////////////////////////////////////////////////////////////////
+
+// Calculator
+
+//Radio Buttons event listeners
+
+document.getElementsByTagName("from")[0].addEventListener("change", function(event){
+  if(event.target.matches("#targetDate")) {
+    console.log("make target date invisible and make target weight apear")
+
+    
+  } else if (event.target.matches("#targetWeight")){
+    console.log("make target weight invisible and make target date apear")
+
+    
+  } else if (event.target.matches("#imperial")){
+    console.log("I will change form to Metric")
+    
+  
+  } else if (event.target.matches("#metric")){
+    console.log("I will change form to Imperial")
+    
+  
+  } else {
+    console.log("radio button unknown")
+    
+  }
+});
+
+//universal function to make things visible
+//universal function to make things invisible
+
+// choose between Target Weight and Target Date
+
+//event listener to replace Metric with imperial and the other way arround

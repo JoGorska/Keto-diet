e725615c5b0edcd010e48c2324704a9566e67ee9
Joanna Gorska <gorska919@gmail.com>
2021-07-25 19:28:32 +0000

    Add variables to identyfy each input field in contact form

---
+++ assets/js/contact.js
@@ -0,0 +1,7 @@
+//declare variables for each input field
+
+var contactName = document.getElementById("contact-name")
+var email = document.getElementById("email")
+var telephone = document.getElementById("telephone")
+var enquiry = document.getElementById("enquiry")
+

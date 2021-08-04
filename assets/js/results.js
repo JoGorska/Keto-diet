86d62b171db6418143445a15f76fe55fb1bb8e0e
Joanna Gorska <gorska919@gmail.com>
2021-08-04 18:42:09 +0000

    Fix issue with embeded video not displaying correctly

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,10 +1,17 @@
 
-https://stackoverflow.com/questions/8648892/how-to-convert-url-parameters-to-a-javascript-object
+//https://stackoverflow.com/questions/8648892/how-to-convert-url-parameters-to-a-javascript-object
 
 var search = location.search.substring(1);
 console.log(JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) }))
 
 console.log(search);
+// this object holds all data that user has input in the calculator form
 
 var allFormData = (JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) }));
-console.log(allFormData)
+console.log(allFormData);
+
+console.log(allFormData["input-name"])
+
+document.getElementById("input-name").innerHTML = allFormData["input-name"]
+
+

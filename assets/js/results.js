f5e4839f1adafcd68427fe24d843cae4f74191fe
Joanna Gorska <gorska919@gmail.com>
2021-08-05 18:56:07 +0000

    Change embeded video size in the cards and minor fix in results.js

--- assets/js/results.js
+++ assets/js/results.js
@@ -10,13 +10,12 @@ console.log(search);
 var allFormData = (JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) }));
 console.log(allFormData);
 
-// this injects user input into the results.html page
 
-document.getElementById("input-name").innerHTML = allFormData["input-name"];
 
 // variables off the string
 var currentWeightStone = allFormData["current-weight-stone"];
-var currentWeightPounds = allFormData["current-weight-pounds"] 
+var currentWeightPounds = allFormData["current-weight-pounds"];
+var currentWeightKg = allFormData["current-weight-kg"]
 
 //test if the data input is imperial or metric measure
 
@@ -30,21 +29,27 @@ function isImperial(stone, pounds) {
     };
 };
 
+// display user name in results.html
 
+document.getElementById("input-name").innerHTML = allFormData["input-name"];
+document.getElementById("input-name2").innerHTML = allFormData["input-name"];
 // display current weight for the user 
 
 if (isImperial(currentWeightStone, currentWeightPounds) === false) {
-    document.getElementById("display-current-weight").innerHTML = allFormData["current-weight-kg"];
-    document.getElementById("display-current-weight2").innerHTML = allFormData["current-weight-kg"];
+    document.getElementById("display-current-weight").innerHTML = `${allFormData["current-weight-kg"]} kg`;
+    document.getElementById("display-current-weight2").innerHTML = `${allFormData["current-weight-kg"]} kg`;
+
 } else if ((isImperial(currentWeightStone, currentWeightPounds) === true) && (currentWeightStone === "")) {
-    document.getElementById("display-current-weight").innerHTML = `${allFormData["current-weight-pounds"]}lbs`;
-    document.getElementById("display-current-weight2").innerHTML = `${allFormData["current-weight-pounds"]}lbs`;
+    document.getElementById("display-current-weight").innerHTML = `${allFormData["current-weight-pounds"]} lbs`;
+    document.getElementById("display-current-weight2").innerHTML = `${allFormData["current-weight-pounds"]} lbs`;
+
 } else if ((isImperial(currentWeightStone, currentWeightPounds) === true) && (currentWeightPounds === "")) {
-    document.getElementById("display-current-weight").innerHTML = `${allFormData["current-weight-stone"]}st`;
-    document.getElementById("display-current-weight2").innerHTML = `${allFormData["current-weight-stone"]}st`;  
+    document.getElementById("display-current-weight").innerHTML = `${allFormData["current-weight-stone"]} st`;
+    document.getElementById("display-current-weight2").innerHTML = `${allFormData["current-weight-stone"]} st`;  
+
 } else {
-    document.getElementById("display-current-weight").innerHTML = `${allFormData["current-weight-stone"]}st ${allFormData["current-weight-pounds"]}lbs`;
-    document.getElementById("display-current-weight2").innerHTML = `${allFormData["current-weight-stone"]}st ${allFormData["current-weight-pounds"]}lbs`;
+    document.getElementById("display-current-weight").innerHTML = `${allFormData["current-weight-stone"]} st ${allFormData["current-weight-pounds"]} lbs`;
+    document.getElementById("display-current-weight2").innerHTML = `${allFormData["current-weight-stone"]} st ${allFormData["current-weight-pounds"]} lbs`;
 };
 
 

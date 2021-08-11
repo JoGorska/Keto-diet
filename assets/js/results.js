f54e05507ad798f4218fc6911bea5fe98a33c83e
Joanna Gorska <gorska919@gmail.com>
2021-08-11 22:36:44 +0000

    Add creating creating a window to contact dietititan from results.html

--- assets/js/results.js
+++ assets/js/results.js
@@ -402,3 +402,52 @@ document.addEventListener("DOMContentLoaded",  function() {
     }
 })
 
+let buttons = document.getElementsByTagName("button");
+      for(let button of buttons) {
+        button.addEventListener("click", function() {
+          if (this.innerHTML === "Dietitian") {
+              console.log("I clicked for dietititan now")
+                var myWindow = window.open("", "", "width=700, height=700");
+              myWindow.document.write(
+                `<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                        <meta name="description" content="">
+                        <meta name="description" content="Change your fuel - website aiming to encourage people to change to keto diet">
+                        <meta name="keywords" content="weight loss, keto, keto diet, diet, target weight, ">
+                        <meta name="author" content="JoGorska">
+
+                        <!--google fonts-->
+                        <link rel="preconnect" href="https://fonts.googleapis.com">
+                        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                        <link href="https://fonts.googleapis.com/css2?family=Baumans&family=Kodchasan:wght@200&display=swap" rel="stylesheet">
+
+                        <!--my stylesheet link-->
+                        <link rel="stylesheet" href="assets/css/style.css">
+                    <title>Be Honest!</title>
+                </head>
+                <body class="background-tape-animated">
+                <div class="my-heading-font my-text-shadow my-text-danger">
+                    <div class="center my-box rounded-corners my-text-center">
+                        <p>${excerciseHours.value} me!</p>
+                
+                        
+                    </div>
+                </div>
+                </body>
+                </html>`);
+              setTimeout(function(){ myWindow.close() }, 3000);
+
+              // moves window from the corner
+              // https://www.w3schools.com/jsref/met_win_moveto.asp
+              function moveWin() {
+                myWindow.moveTo(500, 200);                                  // Moves the new window   
+                myWindow.focus();                                           // Sets focus to the new window
+              }
+              moveWin()
+            }
+        })
+    }
